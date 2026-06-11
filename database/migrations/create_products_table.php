826e026c766143e6db2af19a53ae9cb7613e63ae
mohammed-001 <154636659+mohammed-001@users.noreply.github.com>
2026-06-11 18:22:50 +0300

    Create create_products_table.php

---
+++ database/migrations/create_products_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // products table
+    }
+
+    public function down(): void
+    {
+        // rollback
+    }
+};

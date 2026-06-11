e503f00b71373a62632b7c3fd7ffa0385d05b454
mohammed-001 <154636659+mohammed-001@users.noreply.github.com>
2026-06-11 18:26:14 +0300

    Create products.blade.php

---
+++ resources/views/products.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+
+<h2>Products</h2>
+
+<ul>
+    <li>Gaming Laptop</li>
+    <li>Mechanical Keyboard</li>
+    <li>Wireless Mouse</li>
+</ul>
+
+@endsection

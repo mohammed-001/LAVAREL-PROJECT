a6ca68c075f82ea30789f02de21b07e135167e5f
mohammed-001 <154636659+mohammed-001@users.noreply.github.com>
2026-06-16 15:09:55 +0300

    Create ProductSeeder.php

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+class ProductSeeder
+{
+    public function run()
+    {
+        $products = [
+            ['name' => 'Gaming Laptop', 'price' => 999],
+            ['name' => 'Mechanical Keyboard', 'price' => 79],
+            ['name' => 'Wireless Mouse', 'price' => 39],
+            ['name' => 'Gaming Headset', 'price' => 89]
+        ];
+    }
+}

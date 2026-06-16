95fc58c1155dbd08001d9d937589d9b0022601ef
mohammed-001 <154636659+mohammed-001@users.noreply.github.com>
2026-06-16 15:06:14 +0300

    Create categories.blade.php

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+
+<h2>Categories</h2>
+
+<ul>
+<li>Laptops</li>
+<li>Monitors</li>
+<li>Keyboards</li>
+<li>Mice</li>
+<li>Headphones</li>
+</ul>
+
+</div>
+
+@endsection

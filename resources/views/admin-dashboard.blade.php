c8e38d3d8278ef9db74f008441af502f0ead7f55
mohammed-001 <154636659+mohammed-001@users.noreply.github.com>
2026-06-16 15:07:28 +0300

    Create admin-dashboard.blade.php

---
+++ resources/views/admin-dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+
+<h2>Admin Dashboard</h2>
+
+<p>Total Products: 120</p>
+<p>Total Orders: 450</p>
+<p>Total Customers: 200</p>
+
+</div>
+
+@endsection
